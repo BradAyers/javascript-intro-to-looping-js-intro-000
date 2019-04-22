c5716e42ca395dfe31b317d626938e5ea298a6c3
Brad Ayers <brad@bradayers.com>
2019-04-22 02:36:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
-  var newArray = array;
+  //var newArray = array;
   for (var i = 1; i<26; i++) {
-    newArray = [...newArray,`I am ${i} strange loop${i === 1 ? '' : 's'}.`];
+    array = [...array,`I am ${i} strange loop${i === 1 ? '' : 's'}.`];
   }
   return newArray;
 }
